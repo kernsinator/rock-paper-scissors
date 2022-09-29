e8cb93437a8754ca292e2144795391fe814b7870
Tyler Kerns <tkerns@protonmail.com>
2022-09-29 12:35:01 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # rock-paper-scissors
+
+This is a simple implementation of a rock, paper, scissors game using HTML, CSS, and JavaScript. The first iteration will be console only, with GUI functionality added later.
---
+++ main.js
@@ -0,0 +1,11 @@
+// Display welcome message
+
+// Get input choice from user
+
+// Get random choice for computer
+
+// Compare user and computer choice and find winner
+
+// Display win or lose message to user
+
+// Offer choice to play again
