4407c1df90ded3b11a7248b47479c966d8a84042
Tyler Kerns <tkerns@protonmail.com>
2022-09-29 13:09:58 -0500

    Add playRound functionality and game loop

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 
-function getComputerChoice() {
-    let computerChoice = random(3);
+function getComputerSelection() {
+    let computerSelection = random(3);
     let message;
 
-    switch (computerChoice) {
+    switch (computerSelection) {
         case 0:
-            message = "Rock";
+            message = "rock";
             break;
         case 1:
-            message = "Paper";
+            message = "paper";
             break;
         case 2:
-            message = "Scissors";
+            message = "scissors";
             break;
         default:
             message = "Uh oh, this was not supposed to happen";
@@ -26,11 +26,52 @@ function random(upperBound) {
 
 function playRound(playerSelection, computerSelection) {
     
-}) {
+    console.log(`Computer chose ${computerSelection}`);
+
+    if (playerSelection === computerSelection) {
+        return "It's a draw!";
+    }
+    else if (playerSelection === "rock" ) {
+        if (computerSelection === "paper") {
+            return "You Lose! Paper beats Rock"
+        } else {
+            return "You Win! Rock beats Scissors"
+        }
+    }
+    else if (playerSelection === "paper") {
+        if (computerSelection === "scissors") {
+            return "You Lose! Scissors beats Paper"
+        } else {
+            return "You Win! Paper beats Rock"
+        }
+    }
+    else if (playerSelection === "scissors") {
+        if (computerSelection === "rock") {
+            return "You Lose! Rock beats Scissors"
+        } else {
+            return "You Win! Scissors beats Paper"
+        }
+    }
+    
+    return "There has been an error";
 
 }
 
-getComputerChoice();
+function getPlayerSelection() {
+    let playerSelection = prompt("Please choose Rock, Paper, or Scissors");
+    playerSelection = playerSelection.toLowerCase();
+    return playerSelection;
+}
+
+function game() {
+    console.log("Welcome to this 5-round game of RPS");
+    for (let i = 0; i < 5; i++)  {
+        playRound(getPlayerSelection(), getComputerSelection())
+    }
+}
+
+game();
+
 // Display welcome message
 
 // Get input choice from user
