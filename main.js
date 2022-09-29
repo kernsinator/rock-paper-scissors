75100880768d37e22f2efb9e1c11097e370f29d0
Tyler Kerns <tkerns@protonmail.com>
2022-09-29 12:48:27 -0500

    implement computer move selection

--- main.js
+++ main.js
@@ -1,3 +1,36 @@
+
+function getComputerChoice() {
+    let computerChoice = random(3);
+    let message;
+
+    switch (computerChoice) {
+        case 0:
+            message = "Rock";
+            break;
+        case 1:
+            message = "Paper";
+            break;
+        case 2:
+            message = "Scissors";
+            break;
+        default:
+            message = "Uh oh, this was not supposed to happen";
+    }
+    return message;
+}
+
+// Return random number from 0 - upper boundary that is passed as argument
+function random(upperBound) {
+    return Math.floor(Math.random() * upperBound);
+}
+
+function playRound(playerSelection, computerSelection) {
+    
+}) {
+
+}
+
+getComputerChoice();
 // Display welcome message
 
 // Get input choice from user
